cf6f8954623eed430b21c953b3b1b5ebc540c36b
AYUSHMANPAUDEL <mrdark2064@gmail.com>
2023-12-13 19:57:43 +0545

    Added sign Up feature but Password Hashing is not done yet

--- home/views.py
+++ home/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render, HttpResponse , redirect  
-
+from datetime import datetime
+from home.models import savedata
 # Create your views here.
 def home(request):
-    return render(request , 'index.html')
+    if request.method == "POST":
+        firstname = request.POST.get("firstName")
+        lastname = request.POST.get("lastName")
+        email = request.POST.get("emailAddress")
+        password = request.POST.get("password")
+        birthday = request.POST.get("birthdayDate")
+        phonenumber = request.POST.get("phoneNumber")
+        b = savedata(firstname=firstname,lastname=lastname,email=email,password=password,birthday=birthday,phonenumber=phonenumber,date=datetime.today())
+        b.save()
+    return render(request , 'register.html')
