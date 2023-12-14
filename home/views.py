3a6bc1f83f015d0ecb0c4c9a08ca0395e9b3d89e
Nishant-khatiwada <152992206+Nishant-khatiwada@users.noreply.github.com>
2023-12-14 18:39:54 +0545

    request.html added..

--- home/views.py
+++ home/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render, HttpResponse , redirect
 # Create your views here.
 def home(request):
     return render(request , 'index.html')
+def request(request):
+    return render(request , 'request.html')
