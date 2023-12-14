5ddde912b9bd5e716d75c292a410004f2128e875
Nishant-khatiwada <152992206+Nishant-khatiwada@users.noreply.github.com>
2023-12-14 20:20:37 +0545

    form now are added to database

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Request(models.Model):
+    name=models.CharField( max_length=50)
+    phone=models.CharField( max_length=12)
+    address=models.CharField( max_length=50)
+    email=models.CharField( max_length=50)
+    problem=models.TextField()
+    date=models.DateField()
--- home/views.py
+++ home/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render, HttpResponse , redirect  
-
+from datetime import datetime
+from home.models import Request
 # Create your views here.
 def home(request):
     return render(request , 'index.html')
 def request(request):
+    if request.method=="POST":
+        name=request.POST.get('name')
+        phone=request.POST.get('phone')
+        address=request.POST.get('address')
+        email=request.POST.get('email')
+        problem=request.POST.get('problem')
+        Requests=Request(name=name,phone=phone,address=address, email=email ,problem=problem ,date=datetime.today())
+        Requests.save()
     return render(request , 'request.html')
+
